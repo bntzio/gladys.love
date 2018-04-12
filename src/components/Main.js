44923408d0f659ba168dff3e8cb8e7f0f7068f4a
Enrique Benitez <enriquebenitezjr@gmail.com>
2018-04-12 01:37:50 -0500

    Set up Webpack and React

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Main extends React.Component {
+  render () {
+    return <h1>I ❤️ you Gladys</h1>
+  }
+}
+
+export default Main
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src/components')
+    ],
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.js|.jsx?$/,
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      open: false,
+      port: 3000,
+      proxy: 'http://localhost:8080'
+    })
+  ]
+}
